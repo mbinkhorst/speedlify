a71fd1b40448e20f2f7e9a8ecdbb738de265a3b5
Michel Binkhorst <michel@Hackintoshi9.local>
2020-10-02 12:25:52 +0200

    update

---
+++ _data/sites/heesen-corp.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "Heesen corp", // optional, falls back to object key
+	description: "",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://www.heesenyachts.com"
+	]
+};
